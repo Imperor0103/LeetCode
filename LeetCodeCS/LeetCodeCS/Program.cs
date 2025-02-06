14d364756d5430247b2222722c47d0cc1b2ce920
Imperor <imperor0103@gmail.com>
2025-02-06 10:25:33 +0900

    [addFile] Program
    Main 메서드를 Program에 분리했다

---
+++ LeetCodeCS/LeetCodeCS/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeCS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] heights = new int[] { 2, 1, 5, 6, 2, 3 };
+            Console.WriteLine(Problem_84.FindLargestRect(heights));
+        }
+    }
+}
